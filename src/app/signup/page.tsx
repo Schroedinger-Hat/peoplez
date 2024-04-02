b11c727cf3fe098b70310ca8914980561bfcf632
LobeTia <lobetia@gmail.com>
2024-04-02 20:07:27 +0200

    Started working into multistep

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -1,22 +1,68 @@
-import Link from "next/link"
+'use client'
 
+import Link from "next/link"
 import {Button} from "@/components/ui/button"
 import {Input} from "@/components/ui/input"
 import {Label} from "@/components/ui/label"
+import {useState} from "react";
 
 export default function SignupPage() {
+    const [step, setStep] = useState()
+
     return (
         <>
             <div className="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]">
                 <div className="flex flex-col space-y-2 text-center">
                     <h1 className="text-2xl font-semibold tracking-tight">
-                        Create an account
+                        Activate a Membership
                     </h1>
                     <p className="text-sm text-muted-foreground">
-                        Enter your email below to create your account
+                        Fill the form below to request a membership number for the association: <b>Schroedinger Hat</b>
                     </p>
                 </div>
-                {/*<UserAuthForm/>*/}
+
+                <div className="grid grid-cols-2 gap-2">
+                    <div>
+                        <Label htmlFor="email">Name</Label>
+                        <Input
+                            id="email"
+                            type="email"
+                            placeholder="John"
+                            required
+                        />
+                    </div>
+                    <div>
+                        <Label htmlFor="email">Surname</Label>
+                        <Input
+                            id="email"
+                            type="email"
+                            placeholder="Doe"
+                            required
+                        />
+                    </div>
+                </div>
+                <div className="grid gap-2">
+                    <Label htmlFor="email">Email</Label>
+                    <Input
+                        id="email"
+                        type="email"
+                        placeholder="john.d@example.com"
+                        required
+                    />
+                </div>
+                <div className="grid gap-2">
+                    <Label htmlFor="email">Social Security Code</Label>
+                    <Input
+                        id="email"
+                        type="email"
+                        placeholder="LBRMTT..."
+                        required
+                    />
+                </div>
+                <div className={'float-right'}>
+                    <Button>Next</Button>
+                </div>
+
                 <p className="px-8 text-center text-sm text-muted-foreground">
                     By clicking continue, you agree to our{" "}
                     <Link
