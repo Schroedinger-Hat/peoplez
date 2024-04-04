e238ab22bb089ad0aae34b2d6cf5550cba1482c2
LobeTia <lobetia@gmail.com>
2024-04-04 15:59:37 +0200

    Signup UI

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -1,145 +1,77 @@
 'use client'
 
 import Link from "next/link"
-import {Button} from "@/components/ui/button"
-import {Input} from "@/components/ui/input"
-import {Label} from "@/components/ui/label"
-import {useState} from "react";
-import {Checkbox} from "@/components/ui/checkbox";
-import {useSignUpStore} from "@/app/store/signup";
+import {useForm} from "react-hook-form";
+import {z} from "zod";
+import {zodResolver} from "@hookform/resolvers/zod";
+import {StepForm} from "@/app/signup/stepForm";
+import {DebugDisplay} from "@/components/debug/display";
+
+const formSchema = z.object({
+    name: z
+        .string()
+        .min(2, {
+            message: "Name must be at least 2 characters.",
+        }),
+    surname: z
+        .string()
+        .min(2, {
+            message: "Surname must be at least 2 characters.",
+        }),
+    email: z
+        .string()
+        .email(),
+    socialSecurityCode: z
+        .string()
+        .regex(new RegExp(/^[A-Za-z]{6}[0-9]{2}[A-Za-z]{1}[0-9]{2}[A-Za-z]{1}[0-9]{3}[A-Za-z]{1}$/), 'Invalid format, only italians "codice fiscale" are accepted'),
+    approvedStatute: z
+        .boolean()
+        .refine(val => val, {
+            message: "Please read and accept the statute",
+        })
+})
 
 export default function SignupPage() {
-    const [step, setStep] = useState<number>(1)
-    const state = useSignUpStore()
-
-    function FormStep1() {
-        return (
-            <div className="grid grid-cols-2 gap-2">
-                <div>
-                    <Label htmlFor="name">Name</Label>
-                    <Input
-                        id="name"
-                        type="name"
-                        placeholder="John"
-                        required
-                    />
-                </div>
-                <div>
-                    <Label htmlFor="surname">Surname</Label>
-                    <Input
-                        id="surname"
-                        type="surname"
-                        placeholder="Doe"
-                        required
-                    />
-                </div>
-
-                <div className={'col-span-2 mt-4'}>
-                    <Label htmlFor="email">Email</Label>
-                    <Input
-                        id="email"
-                        type="email"
-                        placeholder="john.d@example.com"
-                        required
-                    />
-                </div>
-
-                <div className={'col-span-2 pt-3'}>
-                    <div className={'flex-row-reverse flex'}>
-                        <Button onClick={() => setStep(2)}>Next</Button>
-                    </div>
-                </div>
-            </div>
-        )
-    }
-
-    function FormStep2() {
-        return (
-            <div className="grid grid-cols-2 gap-2">
-                <div className={'col-span-2'}>
-                    <Label htmlFor="socialSecurityCode">Social Security Code</Label>
-                    <Input
-                        id="socialSecurityCode"
-                        type="socialSecurityCode"
-                        placeholder="LBRMTT..."
-                        required
-                    />
-                </div>
-
-                <div className={'col-span-2 mt-4'}>
-                    <div className={'p-3 border-blue-200 border-2 rounded-2xl'}>
-                        <div className={'flex'}>
-                            <div>
-                                <Checkbox id="terms1"/>
-                            </div>
-                            <div className={'ml-2'}>
-                                <p className={'text-sm font-semibold'}>Statute</p>
-                                <p className={'text-sm'}>I've read and approved the statute</p>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-
-                <div className={'col-span-2 pt-3'}>
-                    <div className={'flex-row-reverse flex'}>
-                        <Button onClick={() => setStep(3)}>Next</Button>
-                    </div>
-                </div>
-            </div>
-        )
-    }
-
-    function FormStep3() {
-        return <>
-            <div className="grid gap-2 animate-in slide-it-from-top">
-                <p className={'text-sm'}>
-                    We're going to redirect you to our PaymentProvider stripe to setup the last details of your
-                    subscription.
-                    Click Next to be redirected
-                </p>
-            </div>
-            <div className={'float-right'}>
-                <Button>Next</Button>
-            </div>
-        </>
-    }
+    const form = useForm<z.infer<typeof formSchema>>({
+        resolver: zodResolver(formSchema),
+        defaultValues: {
+            name: "",
+            surname: "",
+            email: "",
+            socialSecurityCode: "",
+        },
+    })
 
     return (
-        <>
-            <div className="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]">
-                <div className="flex flex-col space-y-2 text-center">
-                    <h1 className="text-2xl font-semibold tracking-tight">
-                        Activate a Membership
-                    </h1>
-                    <p className="text-sm text-muted-foreground">
-                        Fill the form below to request a membership number for the association: <b>Schroedinger Hat</b>
-                    </p>
-                </div>
-
-                <pre>{JSON.stringify(state)}</pre>
-
-                {step === 1 && <FormStep1/>}
-                {step === 2 && <FormStep2/>}
-                {step === 3 && <FormStep3/>}
-
-                <p className="px-8 text-center text-sm text-muted-foreground">
-                    By clicking continue, you agree to our{" "}
-                    <Link
-                        href="/legal-terms-of-service"
-                        className="underline underline-offset-4 hover:text-primary"
-                    >
-                        Terms of Service
-                    </Link>{" "}
-                    and{" "}
-                    <Link
-                        href="/legal/privacy-policy"
-                        className="underline underline-offset-4 hover:text-primary"
-                    >
-                        Privacy Policy
-                    </Link>
-                    .
+        <div className="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]">
+            <div className="flex flex-col space-y-2 text-center">
+                <h1 className="text-2xl font-semibold tracking-tight">
+                    Activate a Membership
+                </h1>
+                <p className="text-sm text-muted-foreground">
+                    Fill the form below to request a membership number for the association: <b>Schroedinger Hat</b>
                 </p>
             </div>
-        </>
+
+            <StepForm form={form}/>
+
+            <p className="px-8 text-center text-sm text-muted-foreground">
+                By clicking continue, you agree to our{" "}
+                <Link
+                    href="/legal/terms-of-service"
+                    className="underline underline-offset-4 hover:text-primary"
+                >
+                    Terms of Service
+                </Link>{" "}
+                and{" "}
+                <Link
+                    href="/legal/privacy-policy"
+                    className="underline underline-offset-4 hover:text-primary"
+                >
+                    Privacy Policy
+                </Link>
+                .
+            </p>
+        </div>
     )
 }
---
+++ src/app/signup/stepForm.tsx
@@ -0,0 +1,138 @@
+'use client'
+
+import {useState} from "react";
+import {Form, FormControl, FormField, FormItem, FormLabel, FormMessage} from "@/components/ui/form";
+import {Input} from "@/components/ui/input";
+import {Button} from "@/components/ui/button";
+import {Checkbox} from "@/components/ui/checkbox";
+import Link from "next/link";
+
+// @ts-ignore
+export function StepForm({form}) {
+    const [step, setStep] = useState<number>(1)
+
+    return (
+        <Form {...form}>
+            {
+                step === 1 && <div className="grid grid-cols-2 gap-2">
+                    <div>
+                        <FormField
+                            control={form.control}
+                            name="name"
+                            render={({field}) => (
+                                <FormItem>
+                                    <FormLabel>Name</FormLabel>
+                                    <FormControl>
+                                        <Input placeholder="John" {...field} />
+                                    </FormControl>
+                                    <FormMessage/>
+                                </FormItem>
+                            )}
+                        />
+                    </div>
+                    <div>
+                        <FormField
+                            control={form.control}
+                            name="surname"
+                            render={({field}) => (
+                                <FormItem>
+                                    <FormLabel>Surname</FormLabel>
+                                    <FormControl>
+                                        <Input placeholder="Doe" {...field} />
+                                    </FormControl>
+                                    <FormMessage/>
+                                </FormItem>
+                            )}
+                        />
+                    </div>
+
+                    <div className={'col-span-2 mt-4'}>
+                        <FormField
+                            control={form.control}
+                            name="email"
+                            render={({field}) => (
+                                <FormItem>
+                                    <FormLabel>Name</FormLabel>
+                                    <FormControl>
+                                        <Input placeholder="john@doe.com" {...field} />
+                                    </FormControl>
+                                    <FormMessage/>
+                                </FormItem>
+                            )}
+                        />
+                    </div>
+
+                    <div className={'col-span-2 pt-3'}>
+                        <div className={'flex-row-reverse flex'}>
+                            <Button
+                                onClick={async () => await form.trigger(['name', 'surname', 'email']) && setStep(2)}>Next</Button>
+                        </div>
+                    </div>
+                </div>
+            }
+            {
+                step === 2 && <div className="grid grid-cols-2 gap-2 animate-in slide-in-from-right-12 duration-300">
+                    <div className={'col-span-2'}>
+                        <FormField
+                            control={form.control}
+                            name="socialSecurityCode"
+                            render={({field}) => (
+                                <FormItem>
+                                    <FormLabel>Social Security Code</FormLabel>
+                                    <FormControl>
+                                        <Input placeholder="LBRMTT..." {...field} />
+                                    </FormControl>
+                                    <FormMessage/>
+                                </FormItem>
+                            )}
+                        />
+                    </div>
+
+                    <div className={'col-span-2 mt-4'}>
+                        <div className={'p-3 border-blue-200 border-2 rounded-2xl'}>
+                            <FormField
+                                control={form.control}
+                                name="approvedStatute"
+                                render={({field}) => (
+                                    <FormItem>
+                                        <div className={'flex'}>
+                                            <FormControl>
+                                                <Checkbox/>
+                                            </FormControl>
+                                            <div className={'ml-2'}>
+                                                <p className={'text-sm font-semibold'}>Statute</p>
+                                                <p className={'text-sm'}>I've read and approved <Link target="_blank" href={'/legal/statute'}>the statute</Link></p>
+                                                <FormMessage/>
+                                            </div>
+                                        </div>
+                                    </FormItem>
+                                )}
+                            />
+                        </div>
+                    </div>
+
+                    <div className={'col-span-2 pt-3'}>
+                        <div className={'flex-row-reverse flex'}>
+                            <Button
+                                onClick={async () => await form.trigger(['socialSecurityCode', 'approvedStatute']) && setStep(3)}>Next</Button>
+                        </div>
+                    </div>
+                </div>
+            }
+            {
+                step === 3 && <div className="grid grid-cols-1 gap-2 animate-in slide-in-from-right-12 duration-300">
+                    <p className={'text-sm'}>
+                        We're going to redirect you to our PaymentProvider stripe to setup the last details of your
+                        subscription.
+                        Click Next to be redirected
+                    </p>
+                    <div className={'col-span-2 pt-3'}>
+                        <div className={'flex-row-reverse flex'}>
+                            <Button>Next</Button>
+                        </div>
+                    </div>
+                </div>
+            }
+        </Form>
+    )
+}
