d2b111bf8a045cdc9a4166ccdc6dbb515905af97
Davide Imola <davide.imola@redcarbon.ai>
2024-04-09 16:17:35 +0200

    feat: fixing be build

--- src/app/api/stripe/membership/manage/route.ts
+++ src/app/api/stripe/membership/manage/route.ts
@@ -1,9 +1,9 @@
-import {type NextApiRequest, NextApiResponse} from "next";
-import {stripe} from "@/services/stripe";
-import {env} from "@/env";
-import {NextResponse} from "next/server";
+import { type NextApiRequest, NextApiResponse } from "next";
+import { stripe } from "@/services/stripe";
+import { env } from "@/env";
+import { NextRequest, NextResponse } from "next/server";
 
-export async function POST(req: NextApiRequest) {
+export async function POST(req: NextRequest) {
   const checkoutSession = await stripe.checkout.sessions.retrieve("xxx");
 
   const portalSession = await stripe.billingPortal.sessions.create({
--- src/app/api/stripe/webhook/route.ts
+++ src/app/api/stripe/webhook/route.ts
@@ -1,38 +1,46 @@
-import {type NextApiRequest, NextApiResponse} from "next";
-import {stripe} from "@/services/stripe";
+import { type NextApiRequest, NextApiResponse } from "next";
+import { stripe } from "@/services/stripe";
 import type Stripe from "stripe";
-import {NextResponse} from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 
 const endpointSecret = "";
 
-export async function POST(req: NextApiRequest) {
-  const signature = req.headers['stripe-signature'] as string;
+export async function POST(req: NextRequest) {
+  const signature = req.headers.get("stripe-signature");
+  if (!signature) {
+    console.log(`⚠️  Webhook signature missing.`);
+    return NextResponse.json({ error: "Missing signature" }, { status: 400 });
+  }
   let event: Stripe.Event;
 
+  const requestBody = await req.text();
   try {
     event = stripe.webhooks.constructEvent(
-      req.body,
+      requestBody,
       signature,
-      endpointSecret
+      endpointSecret,
     );
   } catch (err: any) {
     console.log(`⚠️  Webhook signature verification failed.`, err.message);
-    return NextResponse.json({ error: 'Internal Server Error' }, { status: 500 })
+    return NextResponse.json(
+      { error: "Internal Server Error" },
+      { status: 500 },
+    );
   }
 
   switch (event.type) {
-    case 'customer.subscription.created':
+    case "customer.subscription.created":
       console.log(`💰 Subscription created!`);
       break;
-    case 'customer.subscription.deleted':
+    case "customer.subscription.deleted":
       console.log(`💔 Subscription deleted.`);
       break;
-    case 'customer.subscription.updated':
+    case "customer.subscription.updated":
       console.log(`🔄 Subscription updated.`);
       break;
     default:
       console.log(`❌ Unhandled event type: ${event.type}`);
   }
 
-  return NextResponse.json({received: true}, {status: 200})
+  return NextResponse.json({ received: true }, { status: 200 });
 }
