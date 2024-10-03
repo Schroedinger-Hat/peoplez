f88c7d568c48072a3d1e6e2f6e24297e2fedf623
lobetia <lobetia@gmail.com>
2024-10-03 14:19:56 +0200

    Progress

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,11 +3,11 @@ import type { Config } from "tailwindcss"
 const config = {
   darkMode: ["class"],
   content: [
-    './pages/**/*.{ts,tsx}',
-    './components/**/*.{ts,tsx}',
-    './app/**/*.{ts,tsx}',
-    './src/**/*.{ts,tsx}',
-	],
+    "./pages/**/*.{ts,tsx}",
+    "./components/**/*.{ts,tsx}",
+    "./app/**/*.{ts,tsx}",
+    "./src/**/*.{ts,tsx}",
+  ],
   prefix: "",
   theme: {
     container: {
@@ -74,9 +74,7 @@ const config = {
       },
     },
   },
-  plugins: [
-      require("tailwindcss-animate")
-  ],
+  plugins: [require("tailwindcss-animate")],
 } satisfies Config
 
 export default config
