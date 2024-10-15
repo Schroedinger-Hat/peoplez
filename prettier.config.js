fc79f7526dc934669c4345d225ca4b45552ff81c
Miki Lombardi <miki.lombi@gmail.com>
2024-10-15 19:22:25 +0000

    fix(lint): update prettier and eslint config

--- prettier.config.js
+++ prettier.config.js
@@ -1,7 +1,7 @@
 /** @type {import('prettier').Config & import('prettier-plugin-tailwindcss').PluginOptions} */
 const config = {
     plugins: ["prettier-plugin-tailwindcss"],
-    semi: false
-};
-
-export default config;
+    semi: false,
+}
+  
+export default config
--- prisma/seeds/index.ts
+++ prisma/seeds/index.ts
@@ -1,25 +1,27 @@
-import {PrismaClient} from '@prisma/client'
-
-import {membershipTemplates} from "./models/membershipTemplates";
-import {users} from "./models/users";
+import { PrismaClient } from "@prisma/client"
+import { membershipTemplatesSeeds } from "./models/membershipTemplates"
+import { usersSeeds } from "./models/users"
+import { configurablesSeeds } from "./models/configurables"
 
 const prisma = new PrismaClient()
 
-function seedModel(model: any, data: object[]) {
-    return model.createMany({data, skipDuplicates: true})
+async function seedModel(model: any, data: object[]) {
+  await model.createMany({ data, skipDuplicates: true })
+  return
 }
 
 async function main() {
-    await seedModel(prisma.user, users)
-    await seedModel(prisma.membershipTemplate, membershipTemplates)
+  await seedModel(prisma.user, usersSeeds)
+  await seedModel(prisma.membershipTemplate, membershipTemplatesSeeds)
+  await seedModel(prisma.configurable, configurablesSeeds)
 }
 
 main()
-    .then(async () => {
-        await prisma.$disconnect()
-    })
-    .catch(async (e) => {
-        console.error(e)
-        await prisma.$disconnect()
-        process.exit(1)
-    })
+  .then(async () => {
+    await prisma.$disconnect()
+  })
+  .catch(async (e) => {
+    console.error(e)
+    await prisma.$disconnect()
+    process.exit(1)
+  })
