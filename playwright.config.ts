fc66b64b5aab9c56a20cd9d7cb1223ed608a4063
Miki Lombardi <miki.lombi@gmail.com>
2024-10-30 09:04:15 +0000

    chore: update the playwright config

--- playwright.config.ts
+++ playwright.config.ts
@@ -66,9 +66,9 @@ export default defineConfig({
   ],
 
   /* Run your local dev server before starting the tests */
-  webServer: {
+  /* webServer: {
     command: 'npm run build',
     url: 'http://127.0.0.1:3000',
     reuseExistingServer: !process.env.CI,
-  },
+  }, */
 });
