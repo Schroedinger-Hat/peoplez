9dd1165433f53c207d9a8f7ed9a916e020f0eae9
Patrick Raedler <code@readpato.dev>
2024-11-01 12:11:19 +0100

    refactor(app/page): add basic metadata

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,11 @@ import { redirect, RedirectType } from "next/navigation"
 import { DevPage } from "@/app/devPage"
 import { inDevEnvironment } from "@/lib/envs"
 
+export const metadata = {
+  title: "Homepage",
+  description: "Main page of the website.",
+}
+
 export default async function HomePage() {
   if (inDevEnvironment) return <DevPage />
   else redirect("/members", RedirectType.replace)
